0aec27abf26b007f866bbf2bad85ee6ce7dc3786
macetasmym <108767898+macetasmym@users.noreply.github.com>
2022-07-06 22:33:30 -0300

    Update dataLoader.js

--- js/dataLoader.js
+++ js/dataLoader.js
@@ -73,7 +73,8 @@ fetch("data/data.json")
                 });
             }
             ///index.html
-            if (window.location.pathname == (("/macetas2-v1.2/index.html") || ("/macetas/index.hmtl") || ("/macetas") || ("macetasmym.github.io/macetas/index.html"))) {
+            console.log(window.location.pathname);
+            if (window.location.pathname == "/macetasmym.github.io/macetas/index.html") {
                 var arr = [0,3,11,1,6,7,2,4,5,9,8,10]
                 var HTMLOptions = optionClass.item(i).firstElementChild;
                 HTMLOptions.innerHTML = `$${data.Macetas[arr[i]].Price.NP - 0.01} - $${data.Macetas[arr[i]].Price.PD - 0.01}`;
@@ -81,7 +82,7 @@ fetch("data/data.json")
                 HTMLDescription.innerHTML = `${data.Macetas[arr[i]].Description}`;
             }
             //menu.html
-            if (window.location.pathname == (("/macetas2-v1.2/menu.html") || ("/macetas/menu.html") || ("macetasmym.github.io/macetas/menu.html"))) {
+            if (window.location.pathname == "/macetasmym.github.io/macetas/menu.html") {
                 var arr = [0,3,11,1,6,7,2,4,5,9,8,10]
                 var HTMLOptions = optionClass.item(i).firstElementChild;
                 HTMLOptions.innerHTML = `$${data.Macetas[arr[i]].Price.NP - 0.01} - $${data.Macetas[arr[i]].Price.PD - 0.01}`;
